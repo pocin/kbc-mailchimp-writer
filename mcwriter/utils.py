872b24d3d1b529026a0ac2978b8216c4721631c4
Robin Nemeth <nemeth.robin@gmail.com>
2017-04-19 21:27:07 +0200

    Serializing input table for new lists

--- mcwriter/utils.py
+++ mcwriter/utils.py
@@ -3,6 +3,10 @@
 @author robin@keboola.com
 """
 from collections import defaultdict
+import csv
+import json
+from keboola import docker
+
 
 def serialize_dotted_path_dict(flat_data):
     """Convert fields from csv file into required nested format
@@ -25,4 +29,20 @@ def serialize_dotted_path_dict(flat_data):
             serialized[lvl1][lvl2] = value
         else:
             serialized[key] = value
+    return serialized
+
+def serialize_new_lists_input(path):
+    """Parse the inputs csvfile containing details on new mailing lists
+
+    Arguments:
+        path (str): /path/to/inputs/new_lists.csvfile
+    Returns:
+        a list of serialized dicts in a format that can be used by MC Api
+    """
+    serialized = []
+    with open(path, 'r') as lists:
+        reader = csv.DictReader(lists)
+        for line in reader:
+            serialized_line = serialize_dotted_path_dict(line)
+            serialized.append(serialized_line)
     return serialized
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,7 @@
 import pytest
-from mcwriter.utils import serialize_dotted_path_dict
+from tempfile import NamedTemporaryFile
+from mcwriter.utils import (serialize_dotted_path_dict,
+                            serialize_new_lists_input)
 
 
 def test_serializing_new_lists():
@@ -15,3 +17,25 @@ def test_serializing_new_lists():
         'confirm': True}
     serialized = serialize_dotted_path_dict(flat)
     assert expected == serialized
+
+
+def test_serializing_new_lists_input_csv():
+    # Fake inputs
+    inputs = NamedTemporaryFile(delete=False)
+    inputs.write(b'''name,contact.address,contact.country,confirm
+Robin,Foobar,Czechia,True
+Max,BarBar,Slovakia,False''')
+    inputs.close()
+    serialized = serialize_new_lists_input(inputs.name)
+
+    expected = [{'name': 'Robin',
+                 'contact': {'address': 'Foobar',
+                             'country': 'Czechia'},
+                 'confirm': 'True'},
+                {'name': 'Max',
+                 'contact': {'address': 'BarBar',
+                             'country': 'Slovakia'},
+                 'confirm': 'False'}]
+
+    assert expected[0] == serialized[0]
+    assert expected[1] == serialized[1]
