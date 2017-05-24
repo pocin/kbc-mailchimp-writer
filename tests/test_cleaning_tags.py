622a81642ad246f7760db8e46fe612ef6c8d8d55
Robin Nemeth <nemeth.robin@gmail.com>
2017-05-24 13:09:58 +0200

    cleaning mandatory tags str fields

--- tests/test_cleaning_tags.py
+++ tests/test_cleaning_tags.py
@@ -1,4 +1,5 @@
 import pytest
+from mcwriter.exceptions import MissingFieldError
 from mcwriter.cleaning import clean_and_validate_tags_data
 
 def test_cleaning_tags_minimal_example():
@@ -10,6 +11,15 @@ def test_cleaning_tags_minimal_example():
     expected = data
     cleaned = clean_and_validate_tags_data(data)
     assert cleaned == expected
+
+def test_cleaning_tags_missing_required_field():
+    data = {
+        'name': 'My custom tag',
+    }
+
+    expected = data
+    with pytest.raises(MissingFieldError):
+        clean_and_validate_tags_data(data)
 
 def test_cleaning_tags_some_additional_fields():
     data = {
